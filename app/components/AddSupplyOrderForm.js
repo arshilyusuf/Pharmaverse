508f7954e152c20a7be8e21092f3e43bff15b5e8
arshilyusuf <arshilyusuf@gmail.com>
2025-03-22 15:01:23 +0530

    Update AddSupplyOrderForm.js

--- app/components/AddSupplyOrderForm.js
+++ app/components/AddSupplyOrderForm.js
@@ -17,7 +17,7 @@ export default function AddSupplyOrderForm({ onClose, onOrderAdded }) {
 
   const validateDrugID = async (drugID) => {
     try {
-      const res = await fetch(`${process.env.NEXTAUTH_URL}/api/drugs/${drugID}`);
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/drugs/${drugID}`);
       if (!res.ok) {
         throw new Error("Invalid Drug ID");
       }
@@ -45,7 +45,7 @@ export default function AddSupplyOrderForm({ onClose, onOrderAdded }) {
         return;
       }
       // console.log(formData);
-      const response = await fetch(`${process.env.NEXTAUTH_URL}/api/supplyorders`, {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/supplyorders`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
